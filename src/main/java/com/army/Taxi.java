0e8e38df3c135b2c1305518809aa8e7031e91314
bsevgeny@gmail.com <papakita2009>
2018-05-16 11:54:50 +0300

    Initial commit

---
+++ src/main/java/com/army/Taxi.java
@@ -0,0 +1,20 @@
+package com.army;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/**
+ * @author Evgeny Borisov
+ */
+
+public class Taxi {
+    public static void main(String[] args) {
+       
+
+
+
+
+
+    }
+}
